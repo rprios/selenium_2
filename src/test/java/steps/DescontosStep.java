0abc7f44ef9ce70fbd8dd0f16fe1f16e2e7e2f52
Rodrigo Rios <rodrigo.rios@capgemini.com>
2023-02-01 17:09:58 -0300

    seção 5

--- src/test/java/steps/DescontosStep.java
+++ src/test/java/steps/DescontosStep.java
@@ -2,28 +2,37 @@ package steps;
 
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Quando;
-import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Entao;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class DescontosStep {
+
+    WebDriver driver = new ChromeDriver();
+
     @Dado("^que estou no site da Qazando$")
     public void acessar_site_qazando() {
-        System.out.println("passou1");
+        driver.get("https://qazando.com.br/curso.html");
+        //System.out.println("passou1");
     }
 
     @Quando("^eu preencho meu email$")
     public void eu_preencho_meu_email() {
-        System.out.println("passou2");
+        driver.findElement(By.id("#email"));
+        //System.out.println("passou2");
+        //#email
     }
 
     @Quando("^clico no ganhar cupom$")
     public void clico_no_ganhar_cupom() {
-        System.out.println("passou3");
+        //System.out.println("passou3");
     }
 
-    @Então("^eu vejo o código de desconto$")
-    public void eu_vejo_o_código_de_desconto() {
-        System.out.println("passou4");
+    @Entao("^eu vejo o código de desconto$")
+    public void eu_vejo_o_codigo_de_desconto() {
+        //System.out.println("passou4");
     }
 
 
