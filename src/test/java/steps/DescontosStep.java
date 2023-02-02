e68ab35b108787ae6aa22eea996463325d33fa6a
Rodrigo Rios <rodrigo.rios@capgemini.com>
2023-02-02 17:02:15 -0300

    seção 5 parte 2

--- src/test/java/steps/DescontosStep.java
+++ src/test/java/steps/DescontosStep.java
@@ -1,9 +1,10 @@
 package steps;
 
 import cucumber.api.java.pt.Dado;
-import cucumber.api.java.pt.Quando;
 import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Quando;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,28 +12,42 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class DescontosStep {
 
     WebDriver driver = new ChromeDriver();
+    //Actions actions = new Actions(driver);
+
+
 
     @Dado("^que estou no site da Qazando$")
     public void acessar_site_qazando() {
         driver.get("https://qazando.com.br/curso.html");
-        //System.out.println("passou1");
+        driver.manage().window().maximize();
+        System.out.println("passou1");
     }
 
     @Quando("^eu preencho meu email$")
-    public void eu_preencho_meu_email() {
-        driver.findElement(By.id("#email"));
-        //System.out.println("passou2");
+    public void eu_preencho_meu_email() throws InterruptedException {
+
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("!window.scrollTo(0, 10000)");
+        Thread.sleep(5000);
+
+        driver.findElement(By.id("email")).sendKeys("teste@teste.com");
+        System.out.println("passou2");
         //#email
     }
 
     @Quando("^clico no ganhar cupom$")
-    public void clico_no_ganhar_cupom() {
-        //System.out.println("passou3");
+    public void clico_no_ganhar_cupom() throws InterruptedException {
+        //actions.moveToElement(driver.findElement(By.id("button"))).perform();
+        driver.findElement(By.id("button")).click();
+        System.out.println("passou3");
     }
 
     @Entao("^eu vejo o código de desconto$")
     public void eu_vejo_o_codigo_de_desconto() {
-        //System.out.println("passou4");
+        String texto_cupom = driver.findElement(By.cssSelector("#cupom > h2 > span")).getText();
+        System.out.println(texto_cupom);
+        System.out.println("passou4");
+        driver.quit();
     }
 
 
